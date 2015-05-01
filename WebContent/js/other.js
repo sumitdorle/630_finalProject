11433e5df400e6fed2b83252164fc5583e51b25c
sumitdorle <sumitdorle@gmail.com>
2015-05-01 14:25:10 +0000

    working with jsi

---
+++ WebContent/js/other.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+
+function moveForward(x)
+{
+	move_x = x;
+	//alert(move_x);
+	moveRight = true;
+}
+
+function moveBackward(x)
+{
+	move_x = -x;
+	//alert(move_x);
+	
+	moveLeft = true;
+}
+
+//function moveForward(x, cb){
+//	move_x = x;
+//	moveRight = true;
+	//cb();
+//}
+
+
+
+function dummy()
+{
+	
+}
